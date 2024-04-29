3eb1c1219d84a2ac43ababf8dca17b4230c1bd76
aarthurzop <medeirosartur48@gmail.com>
2024-04-29 20:30:48 -0300

    header complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, CabecalhoComponent]
 })
 export class AppComponent {
   title = 'memoteca';
